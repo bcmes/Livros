4e67c562fd87b1023ef2294801496dad6d2d0da8
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-08 09:57:52 -0400

    Resposta http 404 para busca nao encontrada

--- src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
+++ src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,8 +36,12 @@ public class LivrosResources {
     }
 
     @GetMapping("/{livroId}")
-    public Optional<Livro> buscar(@PathVariable Long livroId) {
-        return  livrosRepository.findById(livroId);
+    public ResponseEntity<?> buscar(@PathVariable Long livroId) {
+        Optional<Livro> livro = livrosRepository.findById(livroId);
+        if(livro.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(livro);
     }
 
     @DeleteMapping("/{livroId}")
