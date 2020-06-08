79c7a62f41113f5dd9aaeda2b6b059f51b3dfb40
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-07 23:29:52 -0400

    @JsonInclude para não enviar campos nulos

--- src/main/java/github/com/brunomeloesilva/domain/Livro.java
+++ src/main/java/github/com/brunomeloesilva/domain/Livro.java
@@ -3,6 +3,10 @@ package github.com.brunomeloesilva.domain;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class Livro {
     
     private Long id;
