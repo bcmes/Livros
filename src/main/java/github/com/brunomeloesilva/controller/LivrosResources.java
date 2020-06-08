45bdbae827ca8be31a9d3002f58b11feea5558aa
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-08 03:56:28 -0400

    Salvando o recurso Livro a partir de um POST

--- src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
+++ src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
@@ -4,19 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.com.brunomeloesilva.domain.Livro;
 import github.com.brunomeloesilva.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 
     @Autowired
     private LivrosRepository livrosRepository;
     
-    @GetMapping("/livros")
+    @GetMapping
     public List<Livro> listar() {
         return livrosRepository.findAll();
+    }
+
+    @PostMapping
+    public void salvar(@RequestBody Livro livro){
+        livrosRepository.save(livro);
     }
 }
