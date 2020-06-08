3278e057162629f961fd726727dc8b7d20d34935
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-08 04:42:04 -0400

    Buscando livro especifico com @PathVariable

--- src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
+++ src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
@@ -1,12 +1,15 @@
 package github.com.brunomeloesilva.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.com.brunomeloesilva.domain.Livro;
@@ -18,14 +21,19 @@ public class LivrosResources {
 
     @Autowired
     private LivrosRepository livrosRepository;
-    
+
     @GetMapping
     public List<Livro> listar() {
         return livrosRepository.findAll();
     }
 
     @PostMapping
-    public void salvar(@RequestBody Livro livro){
+    public void salvar(@RequestBody Livro livro) {
         livrosRepository.save(livro);
+    }
+
+    @GetMapping("/{livroId}")
+    public Optional<Livro> buscar(@PathVariable Long livroId) {
+        return  livrosRepository.findById(livroId);
     }
 }
