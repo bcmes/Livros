0bfe6b642eebf0f73ecf440c68fbb272f0e9648b
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-13 18:23:32 -0400

    Negociação de tipos de retorno com Media Type

--- src/main/java/github/com/brunomeloesilva/controller/AutoresResources.java
+++ src/main/java/github/com/brunomeloesilva/controller/AutoresResources.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,8 @@ public class AutoresResources {
     @Autowired
     private AutoresService autoresService;
 
-    @GetMapping
+    //Via Accept posso solicitar dois formatos diferentes { application/json, application/xml }. 
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE } )
     public ResponseEntity<List<Autor>> listar(){
         return ResponseEntity.status(HttpStatus.OK).body(autoresService.listar());
     }
