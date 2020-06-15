326b6625a763b61b89e66b0c2a3e2f185d77228f
brunomeloesilva <brunomeloesilva@gmail.com>
2020-06-15 16:21:42 -0400

    Obtendo o nome do usuário autenticado

--- src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
+++ src/main/java/github/com/brunomeloesilva/controller/LivrosResources.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -69,6 +71,10 @@ public class LivrosResources {
     /** AREA PARA COMENTARIOS **/
     @PostMapping("/{livroId}/comentarios")
     public ResponseEntity<Void> addComentario(@PathVariable Long livroId, @RequestBody Comentario comentario){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        comentario.setUsuario(authentication.getName());
+
         livrosService.salvarComentario(livroId, comentario);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
         return ResponseEntity.created(uri).build();
